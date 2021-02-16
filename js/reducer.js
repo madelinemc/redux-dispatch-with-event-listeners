5d14ae64ca69d672000bb952163fb3d689551bad
madelinemc <madeline.coons@gmail.com>
2021-02-16 13:48:32 -0600

    commit tree b7c5b96d9b3f967f5c7436d8750048308e380c3e

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,34 @@
 // add code snippets from README
+// let state = {count: 0}
+
+let state;
+
+function reducer(state = {count: 0}, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+}
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
+
+//integrating dispatch with a user event:
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+//Now every time we click, we dispatch an action of type increase. 
+//Dispatch first calls our reducer, which updates our state. Then dispatch renders the updated view.
